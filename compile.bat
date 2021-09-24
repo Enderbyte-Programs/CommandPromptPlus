0f2ed718b7585f216ee0fc712daa5be6cc83bdf1
Enderbyte09 <83879635+Enderbyte09@users.noreply.github.com>
2021-09-23 17:58:43 -0700

    Add files via upload

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-.\pyinstaller --noconfirm --icon=bu.ico --version-file=versionfile.txt --hidden-import plyer.platforms.win.notification BasicUtilities.py
+.\pyinstaller --noconfirm --icon=bu.ico --version-file=versionfile.txt BasicUtilities.py
