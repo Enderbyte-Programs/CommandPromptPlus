329de724aaa222b57c1d4379db0c414e83425a35
Enderbyte09 <83879635+Enderbyte09@users.noreply.github.com>
2022-01-14 16:51:42 -0800

    2.24.4
    -Fixed bug where opening file in notpad did not show filename
    -Fixed bug where pressing X on notpad window would not trigger terminat()
    -Fixed bug where the program could load a corrupt appdata file
    -Basic Utilities will now delete old appdata after porting it
    -Added a new insult :)
    -No non-arg prints before appdata load (more on that in the 2.25 betas)

---
+++ clean.bat
@@ -0,0 +1,4 @@
+del /s /f /q .\BasicUtilities\*
+rmdir /s /q .\BasicUtilities\
+mkdir BasicUtilities
+move BasicUtilities.py .\BasicUtilities\BasicUtilities.py
--- compile.bat
+++ compile.bat
@@ -1,3 +1,6 @@
+move .\BasicUtilities\BasicUtilities.py BasicUtilities.py
+del /s /f /q .\BasicUtilities\*
+rmdir .\BasicUtilities\.temp
 .\pyinstaller --icon=bu.ico --version-file=vf.txt --noconfirm BasicUtilities.py
 .\pyinstaller --icon=bu.ico --version-file=vf.txt --onefile BasicUtilities.py
-PAUSE
+xcopy /s /e .\dist\BasicUtilities .\BasicUtilities
