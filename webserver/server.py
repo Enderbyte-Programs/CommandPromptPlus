6291a0847e6e3fb1ae055294384f6002c721083d
Enderbyte09 <83879635+Enderbyte09@users.noreply.github.com>
2022-07-13 08:20:24 -0700

    Fixed server

--- webserver/server.py
+++ webserver/server.py
@@ -19,6 +19,16 @@ Content-Type: text/html
 </html>
 """
 
+M404 = b"""HTTP/1.1 404 ERROR
+Content-Type: text/html
+
+<html>
+<body>
+<p>Error 404.</p>
+</body>
+</html>
+"""
+
 
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
@@ -31,68 +41,90 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
     """
 
     def handle(self):
-        
-        self.data = self.request.recv(8192).strip()
-        # self.request is the TCP socket connected to the client
-        print(str(datetime.datetime.now()),self.client_address[0]," Connected to server")
-        # Send back a message in html
-        self.request.sendall(MESSAGE)
         try:
-            vd[str(self.client_address[0])] += 1
-        except:
-            vd[str(self.client_address[0])] = 1
-        print(len(vd),"IP addresses have connected to this server")
-        with open("users.json","w+") as f:
-            f.write(json.dumps(vd))
-        if len(self.data) < 10000:
-            with open("data.txt","a+") as g:
-                g.write(self.client_address[0])
-                g.write(" says ")
-                g.write(str(self.data))
-                g.write("\n")
-        else:
-            print("Server recieved packet greater than 10 KB!")
-        with open("server.log","a+") as l:
-            l.write(str(datetime.datetime.now()))
-            l.write(" ")
-            l.write(self.client_address[0])
-            l.write("\n")
-        #Accept Basic Utilities data
-        
-        if "&&BU" in str(self.data).split("$")[0].replace("b'",""):
-            budata = str(self.data).split("$")
+            self.data = self.request.recv(8192).strip()
+            # self.request is the TCP socket connected to the client
+            print(str(datetime.datetime.now()),self.client_address[0]," Connected to server")
+            # Send back a message in html
+            
             try:
-                buver = budata[1]
-                buos = budata[2]
-            except IndexError:
-                print("Invalid basic utilities string")
+                vd[str(self.client_address[0])] += 1
+            except:
+                vd[str(self.client_address[0])] = 1
+            print(len(vd),"IP addresses have connected to this server")
+            with open("users.json","w+") as f:
+                f.write(json.dumps(vd))
+            if len(self.data) < 10000:
+                with open("data.txt","a+") as g:
+                    g.write(self.client_address[0])
+                    g.write(" says ")
+                    g.write(str(self.data))
+                    g.write("\n")
             else:
-                budict["lastupdated"] = str(datetime.datetime.now())
-                
-                nu = False
-                i = -1
-                if budict["ulist"]:
-                    for user in budict["ulist"]:
-                        i += 1
-                        if user["ip"] == self.client_address[0]:
-                            budict["ulist"][i]["joinlist"].append(str(datetime.datetime.now()))
-                            budict["ulist"][i]["jtimes"] = len(user["joinlist"])
-                            for os in user["oses"]:
-                                if buos != os:
-                                    budict["ulist"][i]["oses"].append(buos)
-                            for version in user["versions"]:
-                                if version != buver:
-                                    budict["ulist"][i]["versions"].append(buver)
-                            break
-                        else:
-                            nu = True
+                print("Server recieved packet greater than 10 KB!")
+            with open("server.log","a+") as l:
+                l.write(str(datetime.datetime.now()))
+                l.write(" ")
+                l.write(self.client_address[0])
+                l.write("\n")
+            #Accept Basic Utilities data
+            
+            if "&&BU" in str(self.data).split("$")[0].replace("b'",""):
+                budata = str(self.data).split("$")
+                try:
+                    buver = budata[1]
+                    buos = budata[2]
+                except IndexError:
+                    print("Invalid basic utilities string")
                 else:
-                    nu = True
-                if nu:#New user setup
-                    print("Setting up new user")
-                    budict["ulist"].append({"ip":self.client_address[0],"joinlist":[str(datetime.datetime.now())],"jtimes":1,"oses":[buos],"versions":[buver]})
-                with open("bu.json","w+") as f:
-                    f.write(json.dumps(budict))
+                    budict["lastupdated"] = str(datetime.datetime.now())
+                    
+                    nu = False
+                    i = -1
+                    if budict["ulist"]:
+                        for user in budict["ulist"]:
+                            i += 1
+                            if user["ip"] == self.client_address[0]:
+                                budict["ulist"][i]["joinlist"].append(str(datetime.datetime.now()))
+                                budict["ulist"][i]["jtimes"] = len(user["joinlist"])
+                                for os in user["oses"]:
+                                    if buos != os:
+                                        budict["ulist"][i]["oses"].append(buos)
+                                for version in user["versions"]:
+                                    if version != buver:
+                                        budict["ulist"][i]["versions"].append(buver)
+                                break
+                            else:
+                                nu = True
+                    else:
+                        nu = True
+                    if nu:#New user setup
+                        print("Setting up new user")
+                        budict["ulist"].append({"ip":self.client_address[0],"joinlist":[str(datetime.datetime.now())],"jtimes":1,"oses":[buos],"versions":[buver]})
+                    with open("bu.json","w+") as f:
+                        f.write(json.dumps(budict))
+            else:
+                
+                self.apidat = self.data.split(b" ")[1].decode("utf-8")
+                print(self.apidat)
+                if self.apidat == "/":
+                    self.request.sendall(MESSAGE)
+                elif self.apidat == "/favicon.ico":
+                    with open("favicon.ico","rb") as r:
+                        self.request.sendall(r.read())
+                else:
+                    self.request.sendall(M404)
+        except Exception as e:
+            self.request.sendall(f"""
+HTTP/1.1 400 ERROR
+Content-Type: text/html
+
+<html>
+<body>
+<p>Critical Server Error {e}</p>
+</body>
+</html>
+""".encode("utf-8"))
 
 if __name__ == "__main__":
     HOST, PORT = "192.168.1.170", 10223
